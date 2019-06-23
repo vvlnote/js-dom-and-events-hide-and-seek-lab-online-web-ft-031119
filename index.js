50119dd93295bf93224aa7c0e1ded10a4865c550
Joy Lin <vvlnote@hotmail.com>
2019-06-23 16:42:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget() {
-  return document.querySelector('#nested.target');
+  return document.querySelector('div#nested.target');
+}
+
+function increaseRankBy(n) {
+  let elements = document.querySelectorAll('ul.ranked-list li');
+  for(let i = 0; i < elements.length; i++) {
+    let value = parseInt(elements[i].innerHTML);
+    elements[i].innerHTML = (value + n).toString();
+  }
 }
